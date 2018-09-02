689aa7aa712e69f81bb42124c6328885e2b81378
Ali Sufyan <alisufyan327@gmail.com>
2018-09-02 14:26:48 +0500

    Counter increment added in list of counters

---
+++ src/components/counter.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    value: this.props.value,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ value: this.state.value + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => {
+            this.handleIncrement({ id: 1 });
+          }}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {/* <ul>{this.renderTags()}</ul> */}
+      </div>
+    );
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return "There are no tags!";
+
+    return this.state.tags.map(tag => <li key={tag}>{tag}</li>);
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
+  }
+}
+
+export default Counter;
